24414f923cc676615c53ccab4f3a8eabf13e6fe9
Lee Korff-Korn <leekk270@gmail.com>
2023-10-05 04:27:13 +0200

    fixed payment_method_types

--- app/api/stripe-subscription/route.ts
+++ app/api/stripe-subscription/route.ts
@@ -34,7 +34,8 @@ export async function GET() {
     const stripeSession = await stripe.checkout.sessions.create({
       success_url: settingsUrl,
       cancel_url: settingsUrl,
-      payment_method_types: ['card', 'cashapp', 'paypal'],
+      payment_method_types: ['card'],
+      // payment_method_types: ['card', 'cashapp', 'paypal'],
       mode: 'subscription',
       billing_address_collection: 'auto',
       customer_email: user.emailAddresses[0].emailAddress,
