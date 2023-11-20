41bed4ebc72446a730c56ebc37e9d2e5f3a94475
Lee Korff-Korn <leekk270@gmail.com>
2023-11-20 17:46:10 +0100

    added ignoredRoutes

--- middleware.ts
+++ middleware.ts
@@ -4,13 +4,14 @@ import { NextResponse, type NextRequest } from 'next/server';
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-// export default authMiddleware({
-//   publicRoutes: ['/', '/api/stripe-webhook', '/api/cron'],
-// });
-
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
+export default authMiddleware({
+  ignoredRoutes: ['/'],
+  publicRoutes: ['/', '/api/stripe-webhook', '/api/cron'],
 });
+
+// export default withClerkMiddleware((req: NextRequest) => {
+//   return NextResponse.next();
+// });
 
 // export default authMiddleware({});
 export const config = {
